a8880281506b7ba63fce6bb7235007ade855815d
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2020-01-19 09:44:57 +0100

    never include the preamble for text/pgf mime

--- src/LatexWithPGFPlots.jl
+++ src/LatexWithPGFPlots.jl
@@ -6,8 +6,9 @@ function __init__()
     IJulia.register_mime(MIME"text/pgf"())
 end
 
-Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX._SHOWABLE) = print_tex(io, p)
+Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX._SHOWABLE) =
+    print_tex(io, p, include_preamble=false)
 Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX.AxisLike) =
-    print_tex(io, TikzPicture(p))
+    print_tex(io, TikzPicture(p), include_preamble=false)
 
 end # module
