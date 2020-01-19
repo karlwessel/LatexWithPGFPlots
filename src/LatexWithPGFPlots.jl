1c8db06addd63caa178ba173576bb86d8f16e32d
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2020-01-19 11:20:17 +0100

    first version with Plots support

--- src/LatexWithPGFPlots.jl
+++ src/LatexWithPGFPlots.jl
@@ -1,14 +1,25 @@
 module LatexWithPGFPlots
 using PGFPlotsX
 using IJulia
+using Plots
 
 function __init__()
     IJulia.register_mime(MIME"text/pgf"())
 end
 
+# for PGFPlotsX
 Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX._SHOWABLE) =
-    print_tex(io, p, include_preamble=false)
+    PGFPlotsX.print_tex(io, p)
 Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX.AxisLike) =
-    print_tex(io, TikzPicture(p), include_preamble=false)
+    PGFPlotsX.print_tex(io, TikzPicture(p))
+Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX.TikzDocument) =
+    PGFPlotsX.print_tex(io, p, include_preamble=false)
 
+# for Plots
+function Plots._ijulia__extra_mime_info!(
+        plt::Plots.Plot{Plots.PGFPlotsXBackend},
+        out::Dict)
+    out["text/pgf"] = sprint(show, MIME("text/pgf"), plt.o.the_plot)
+    out
+end
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,32 @@ using LatexWithPGFPlots
 using PGFPlotsX
 using IJulia
 using Test
+using Plots
 
 @test MIME("text/pgf") in IJulia.ijulia_mime_types
 
-p = @pgf Axis()
-@test "text/pgf" in keys(IJulia.display_dict(p))
+@testset "PGFPlotsX integration" begin
+    p = @pgf Axis()
+    @test "text/pgf" in keys(IJulia.display_dict(p))
 
-p = @pgf TikzPicture(p)
-@test "text/pgf" in keys(IJulia.display_dict(p))
+    p = @pgf TikzPicture(p)
+    @test "text/pgf" in keys(IJulia.display_dict(p))
+
+    p = @pgf TikzDocument(p)
+    d = IJulia.display_dict(p)
+    @test "text/pgf" in keys(d)
+    @test !endswith(d["text/pgf"], "\\end{document}\n")
+end
+
+#= tests do not work because Plots and IJulia behave differently outside a
+notebook
+
+@testset "Plots integration" begin
+    p = Plots.plot([1,3,4])
+    d = Plots._ijulia_display_dict(p)
+    @test "text/pgf" in keys(d)
+    @test !endswith(d["text/pgf"], "\\end{document}\n")
+end
+=#
+
 end
