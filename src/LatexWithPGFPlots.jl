15a559bd754987a7585c46c0f9c4af0dc968ddc7
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2020-01-19 18:29:49 +0100

    fixed conflicting import

--- src/LatexWithPGFPlots.jl
+++ src/LatexWithPGFPlots.jl
@@ -1,7 +1,7 @@
 module LatexWithPGFPlots
-using PGFPlotsX
+import PGFPlotsX
 import PGFPlots
-using IJulia
+import IJulia
 import Plots
 
 function __init__()
@@ -20,7 +20,7 @@ Base.show(io::IO, ::MIME"text/pgf", p::PGFPlotsX.TikzDocument) =
 function Base.show(f::IO, ::MIME"text/pgf", p::PGFPlots.Plottable)
     tmp, _ = mktemp()
     file = "$(tmp).tex"
-    save(file, p, include_preamble=false)
+    PGFPlots.save(file, p, include_preamble=false)
     tex = read(file, String)
     print(f, tex)
 end
